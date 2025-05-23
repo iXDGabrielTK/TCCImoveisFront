64ce29e6f73efae2e5ee05b50ff33c059cc5accd
CaioFabric10 <caiofabricio07@gmail.com>
2025-05-23 18:56:32 -0300

    2.3.1 - melhorei cadastro de imobiliaria

--- src/components/ImobiliariaForm.tsx
+++ src/components/ImobiliariaForm.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import api from '../services/api';
 import { useNavigate } from 'react-router-dom';
-import '../styles/RegisterImobiliaria.css';
+import '../styles/RegisterImobiliaria.css'
+import InputMask from 'react-input-mask';
 
 const ImobiliariaForm: React.FC = () => {
     const [nome, setNome] = useState('');
@@ -35,24 +36,116 @@ const ImobiliariaForm: React.FC = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="register-imobiliaria-form">
-            <h2>Cadastrar Imobiliária</h2>
+        <form onSubmit={handleSubmit} className="imobiliaria-form-grid">
+            <h2>Dados de cadastro</h2>
 
-            <div className="input-group">
-                <input placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} required />
-            </div>
-            <div className="input-group">
-                <input placeholder="Razão Social" value={razaoSocial} onChange={(e) => setRazaoSocial(e.target.value)} required />
-            </div>
-            <div className="input-group">
-                <input placeholder="CNPJ" value={cnpj} onChange={(e) => setCnpj(e.target.value)} required />
-            </div>
-            <div className="input-group">
-                <input placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-            </div>
-            <div className="input-group">
-                <input placeholder="CEP" value={cep} onChange={(e) => setCep(e.target.value)} required />
-            </div>
+            <fieldset>
+                <legend>Sobre você</legend>
+                <div className="grid">
+                    <div className="form-group">
+                        <label htmlFor="nome">Nome completo sem abreviações</label>
+                        <input
+                            id="nome"
+                            value={nome}
+                            onChange={(e) => setNome(e.target.value)}
+                            placeholder="Insira seu nome"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="cargo">Seu cargo na imobiliária</label>
+                        <input
+                            id="cargo"
+                            placeholder="Insira seu cargo atual"
+                        />
+                    </div>
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <legend>Sobre a empresa</legend>
+                <div className="grid" style={{ alignItems: "end" }}>
+                    <div className="form-group">
+                        <label htmlFor="razaoSocial">Razão social sem abreviações</label>
+                        <input
+                            id="razaoSocial"
+                            value={razaoSocial}
+                            onChange={(e) => setRazaoSocial(e.target.value)}
+                            placeholder="Insira a razão social"
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="cnpj">CNPJ</label>
+                        <InputMask
+                            mask="99.999.999/9999-99"
+                            maskChar="_"
+                            value={cnpj}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCnpj(e.target.value)}
+                        >
+                            {(inputProps: React.InputHTMLAttributes<HTMLInputElement>) => (
+                                <input
+                                    {...inputProps}
+                                    type="text"
+                                    id="cnpj"
+                                    placeholder="Insira o CNPJ"
+                                    inputMode="numeric"
+                                    pattern="\d*"
+                                />
+                            )}
+                        </InputMask>
+
+
+                    </div>
+
+
+                    <div className="form-group">
+                        <label htmlFor="email">E-mail</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Insira seu e-mail"
+                            required
+                        />
+                    </div>
+
+                    <div className="cep-wrapper">
+                        <div className="form-group">
+                            <label htmlFor="cep">CEP</label>
+                            <InputMask
+                                mask="99999-999"
+                                maskChar="_"
+                                value={cep}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCep(e.target.value)}
+                            >
+                                {(inputProps) => (
+                                    <input
+                                        {...inputProps}
+                                        type="text"
+                                        id="cep"
+                                        placeholder="Digite seu CEP"
+                                        inputMode="numeric"
+                                        pattern="\d*"
+                                    />
+                                )}
+                            </InputMask>
+                        </div>
+
+                        <a
+                            href="https://buscacepinter.correios.com.br/app/endereco/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="cep-link"
+                        >
+                            não sei o meu CEP
+                        </a>
+                    </div>
+
+                </div>
+            </fieldset>
 
             <button type="submit">Cadastrar</button>
 
