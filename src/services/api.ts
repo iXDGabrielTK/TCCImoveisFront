23bce9012c834b1ec328d5f76d2bc533636a4c4e
iXDGabrielTK <gabrielferraritanaka@gmail.com>
2025-06-28 21:49:30 -0300

    2.7 - Modificacao para subir a aplicacao na nuvem

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
